a158596a8eced87010ca1d160ca05b79852540ba
furkaner <furkan08_@hotmail.com>
2022-06-16 02:23:24 +0300

    checkIfCorrect func added

--- src/store/slices/createGameSlice.js
+++ src/store/slices/createGameSlice.js
@@ -1,20 +1,28 @@
-import Service from '../../services/api'
-const createGameSlice =(set,get)=>({
-    textData:[],
-    duration:60,
-    correctAnswers:0,
-    wrongAnswers:0,
-    timer: ()=>{
-      set({duration:this.duration-1})
-    },
-    reStartTheGame:()=>{
-      set({duration:60,correctAnswers:0,wrongAnswers:0})
-    },
-    fetchText: async ()=>{
-      const  response = await Service.getText()
-    set({textData: await response[0].split(' ')});
-    },
-
-})
+import Service from "../../services/api";
+const createGameSlice = (set, get) => ({
+  textData: [],
+  duration: 60,
+  correctAnswers: 0,
+  wrongAnswers: 0,
+  checkIfCorrect: (result) => {
+    if (result) {
+      set({ correctAnswers: get().correctAnswers + 1 });
+    } else {
+      set({ wrongAnswers: get().wrongAnswers + 1 });
+    }
+    console.log(get().correctAnswers);
+    console.log(get().wrongAnswers);
+  },
+  timer: () => {
+    set({ duration: get().duration - 1 });
+  },
+  reStartTheGame: () => {
+    set({ duration: 60, correctAnswers: 0, wrongAnswers: 0 });
+  },
+  fetchText: async () => {
+    const response = await Service.getText();
+    set({ textData: await response[0].split(" ") });
+  },
+});
 
 export default createGameSlice;
